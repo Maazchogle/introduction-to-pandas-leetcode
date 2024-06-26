57ca77c904a8678ec58d52e86d9d53f651e3fa0e
Maazchogle <110540925+Maazchogle@users.noreply.github.com>
2024-06-26 23:21:39 +0530

    Update 2881. Create a New Column.py

--- 2881. Create a New Column.py
+++ 2881. Create a New Column.py
@@ -1,47 +1,66 @@
-2881. Create a New Column
-Solved
-Easy
-Companies
-Hint
-DataFrame employees
-+-------------+--------+
-| Column Name | Type.  |
-+-------------+--------+
-| name        | object |
-| salary      | int.   |
-+-------------+--------+
-A company plans to provide its employees with a bonus.
+# 2881. Create a New Column
+# Solved
+# Easy
+# Companies
+# Hint
+# DataFrame employees
+# +-------------+--------+
+# | Column Name | Type.  |
+# +-------------+--------+
+# | name        | object |
+# | salary      | int.   |
+# +-------------+--------+
+# A company plans to provide its employees with a bonus.
 
-Write a solution to create a new column name bonus that contains the doubled values of the salary column.
+# Write a solution to create a new column name bonus that contains the doubled values of the salary column.
 
-The result format is in the following example.
+# The result format is in the following example.
 
  
 
-Example 1:
+# Example 1:
 
-Input:
-DataFrame employees
-+---------+--------+
-| name    | salary |
-+---------+--------+
-| Piper   | 4548   |
-| Grace   | 28150  |
-| Georgia | 1103   |
-| Willow  | 6593   |
-| Finn    | 74576  |
-| Thomas  | 24433  |
-+---------+--------+
-Output:
-+---------+--------+--------+
-| name    | salary | bonus  |
-+---------+--------+--------+
-| Piper   | 4548   | 9096   |
-| Grace   | 28150  | 56300  |
-| Georgia | 1103   | 2206   |
-| Willow  | 6593   | 13186  |
-| Finn    | 74576  | 149152 |
-| Thomas  | 24433  | 48866  |
-+---------+--------+--------+
-Explanation: 
-A new column bonus is created by doubling the value in the column salary.
+# Input:
+# DataFrame employees
+# +---------+--------+
+# | name    | salary |
+# +---------+--------+
+# | Piper   | 4548   |
+# | Grace   | 28150  |
+# | Georgia | 1103   |
+# | Willow  | 6593   |
+# | Finn    | 74576  |
+# | Thomas  | 24433  |
+# +---------+--------+
+# Output:
+# +---------+--------+--------+
+# | name    | salary | bonus  |
+# +---------+--------+--------+
+# | Piper   | 4548   | 9096   |
+# | Grace   | 28150  | 56300  |
+# | Georgia | 1103   | 2206   |
+# | Willow  | 6593   | 13186  |
+# | Finn    | 74576  | 149152 |
+# | Thomas  | 24433  | 48866  |
+# +---------+--------+--------+
+# Explanation: 
+# A new column bonus is created by doubling the value in the column salary.
+
+solution:
+
+import pandas as pd
+
+def createBonusColumn(employees: pd.DataFrame) -> pd.DataFrame:
+    employees['bonus'] = employees['salary'] * 2
+    return employees
+
+data = {
+    'name': ['Piper', 'Grace', 'Georgia', 'Willow', 'Finn', 'Thomas'],
+    'salary': [4548, 28150, 1103, 6593, 74576, 24433]
+}
+
+employees = pd.DataFrame(data)
+result_table = createBonusColumn(employees)
+
+print(result_table)
+
