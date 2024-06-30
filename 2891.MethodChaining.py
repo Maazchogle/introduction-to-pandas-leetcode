000d4e53342905b0d209e7d5f094316e6b675885
Maazchogle <110540925+Maazchogle@users.noreply.github.com>
2024-06-30 12:16:11 +0530

    Create 2891.MethodChaining.py

---
+++ 2891.MethodChaining.py
@@ -0,0 +1,71 @@
+2891. Method Chaining
+Solved
+Easy
+Companies
+DataFrame animals
++-------------+--------+
+| Column Name | Type   |
++-------------+--------+
+| name        | object |
+| species     | object |
+| age         | int    |
+| weight      | int    |
++-------------+--------+
+Write a solution to list the names of animals that weigh strictly more than 100 kilograms.
+
+Return the animals sorted by weight in descending order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+DataFrame animals:
++----------+---------+-----+--------+
+| name     | species | age | weight |
++----------+---------+-----+--------+
+| Tatiana  | Snake   | 98  | 464    |
+| Khaled   | Giraffe | 50  | 41     |
+| Alex     | Leopard | 6   | 328    |
+| Jonathan | Monkey  | 45  | 463    |
+| Stefan   | Bear    | 100 | 50     |
+| Tommy    | Panda   | 26  | 349    |
++----------+---------+-----+--------+
+Output: 
++----------+
+| name     |
++----------+
+| Tatiana  |
+| Jonathan |
+| Tommy    |
+| Alex     |
++----------+
+Explanation: 
+All animals weighing more than 100 should be included in the results table.
+Tatiana's weight is 464, Jonathan's weight is 463, Tommy's weight is 349, and Alex's weight is 328.
+The results should be sorted in descending order of weight.
+
+Solution:
+import pandas as pd
+
+def findHeavyAnimals(animals):
+    return animals[animals['weight'] > 100].sort_values(by='weight', ascending=False)[['name']]
+
+# Sample input data for testing
+data = {
+    'name': ['Tatiana', 'Khaled', 'Alex', 'Jonathan', 'Stefan', 'Tommy'],
+    'species': ['Snake', 'Giraffe', 'Leopard', 'Monkey', 'Bear', 'Panda'],
+    'age': [98, 50, 6, 45, 100, 26],
+    'weight': [464, 41, 328, 463, 50, 349]
+}
+
+# Create DataFrame
+animals = pd.DataFrame(data)
+
+# Get the names of heavy animals
+result_table = findHeavyAnimals(animals)
+
+# Display the result
+print(result_table)
